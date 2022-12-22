91ab95dc6eb44fd01beb142a1d7df6f88bc350a0
trxy2221 <patrick20131208@gmail.com>
2022-12-22 20:58:49 +0800

    wad

--- personal/demo.js
+++ personal/demo.js
@@ -17,23 +17,5 @@ function showMenuItems (event) {
 
 window.onload = function () {
     let figures = $("figure>img");
-    // while(1) setTimeout(function(){ showMenuItems() }, 3000)
     figures.on('click', showMenuItems);
 };
-
-// window.onload = function () {
-//     let figures = $("img");
-//     let alle = $("*").not("div:first");
-//     figures.on('click', showMenuItems);
-//     alle.on('click', hideMenuItems);
-// };
-
-
-// function hideMenuItems () {
-//     let menuItems = 
-//         document.querySelectorAll('div:not(:first-of-type)');
-//     for(let i=0; i<menuItems.length; i++) {
-//         let item = menuItems[i];
-//         item.style.display = 'none';
-//     }
-// }
