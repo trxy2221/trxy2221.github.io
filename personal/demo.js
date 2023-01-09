a5a2fdcf2024410f947303a4341cdeed00568b33
trxy2221 <patrick20131208@gmail.com>
2023-01-09 19:54:30 +0800

    fes

--- personal/demo.js
+++ personal/demo.js
@@ -3,14 +3,23 @@ let cur = 0;
 function showMenuItems (event) {
     // event.stopPropagation();
     console.log(cur);
-    let menuItems = document.querySelectorAll('figure>img');
+    // let menuItems = document.querySelectorAll('figure>img');
+    let menuItems = $('figure>img');
     if(cur!=menuItems.length-1) {
-        menuItems[cur].style.display = 'none';
-        menuItems[cur+1].style.display = 'inline-block';
+        $(menuItems[cur]).removeClass('cur');
+        $(menuItems[cur]).addClass('ncur');
+        // menuItems[cur].style.display = 'none';
+        $(menuItems[cur+1]).removeClass('ncur');
+        $(menuItems[cur+1]).addClass('cur');
+        // menuItems[cur+1].style.display = 'inline-block';
         cur++;
     } else {
-        menuItems[cur].style.display = 'none';
-        menuItems[0].style.display = 'inline-block';
+        $(menuItems[cur]).removeClass('cur');
+        $(menuItems[cur]).addClass('ncur');
+        // menuItems[cur].style.display = 'none';
+        $(menuItems[0]).removeClass('ncur');
+        $(menuItems[0]).addClass('cur');
+        // menuItems[0].style.display = 'inline-block';
         cur = 0;
     }
 }
